0ff8806ea161b1fa016fa1b75d0464cce5d94a7c
jhlee <jhlee@spicusinc.com>
2022-05-20 19:15:02 +0900

    Edit Service

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const dotenv = require('dotenv');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var wordsRouter = require('./routes/words');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const wordsRouter = require('./routes/words');
 
-var mysql = require('./lib/mysql');
-var results = mysql.query('SELECT * FROM WORDS');
-console.log("results:", results);
-
-var app = express();
+dotenv.config();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.set('env', 'development');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,12 +29,12 @@ app.use('/users', usersRouter);
 app.use('/words', wordsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -42,6 +42,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 });
 
 module.exports = app;
--- routes/words.js
+++ routes/words.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var mysql = require('../lib/mysql');
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/:seq', function(req, res, next) {
+router.get('/:seq', async (req, res, next) => {
   const word_seq = req.params.seq;
-  const word = words.filter(data => data.seq == word_seq);
-  //res.json({ok: true, word: word});
-  res.send(word[0].korean);
+  const results = mysql.query(`SELECT * FROM WORDS WHERE SEQ = ${word_seq}`);
+  console.log(results);
+  res.send(results);
 });
 
 module.exports = router;
