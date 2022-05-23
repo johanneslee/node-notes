6ae124e15751b6930dc5f4573ab7d1b6dfb36d67
jhlee <jhlee@spicusinc.com>
2022-05-23 19:12:55 +0900

    Edit Service

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/', usersRouter);
-app.use('/', wordsRouter);
+app.use('/words', wordsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/words.js
+++ routes/words.js
@@ -3,19 +3,21 @@ var mysql = require('../lib/mysql');
 
 var router = express.Router();
 
-router.post('words', async (req, res, next) => {
-  const korean = req.params.korean;
-  const english = req.params.english;
-  const description = req.params.description;
-  const results = mysql.query(`INSERT INTO WORDS (KOREAN, ENGLISH, DESCRIPTION) VALUES ('${korean}','${english}','${description}'`);
-  console.log(results);
+router.post('', async (req, res, next) => {
+  const korean = req.get('korean');
+  const english = req.get('english');
+  const description = req.get('description');
+
+  const results = mysql.query(`INSERT INTO WORDS (KOREAN, ENGLISH, DESCRIPTION) VALUES ('${korean}','${english}','${description}')`);
+
   res.send(results);
 });
 
-router.get('words/:seq', async (req, res, next) => {
+router.get('/:seq', async (req, res, next) => {
   const word_seq = req.params.seq;
+
   const results = mysql.query(`SELECT * FROM WORDS WHERE SEQ = ${word_seq}`);
-  console.log(results);
+
   res.send(results);
 });
 
