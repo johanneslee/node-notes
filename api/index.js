733c5bf3a973491bf5d813c78c8dcf9f491fc8f8
JohannesLee <sj202117@gmail.com>
2022-05-25 01:17:30 +0900

    Optimize for Vercel

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
-const dotenv = require('dotenv');
-const indexRouter = require('../routes/index');
-const usersRouter = require('../routes/users');
-const wordsRouter = require('../routes/words');
-
-dotenv.config();
 const app = require('express')();
+const { v4 } = require('uuid');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/words', wordsRouter);
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
 module.exports = app;
